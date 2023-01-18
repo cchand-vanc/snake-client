2f71185f7258826a328dca6550ef03def95f1ae4
cchand-vanc <cmchandler@live.co.uk>
2023-01-18 15:23:06 -0800

    added name to appear when first connected to server

--- client.js
+++ client.js
@@ -6,9 +6,20 @@ const connect = function () {
     port: "50541"
   });
 
+  conn.on("connect", () => {
+    console.log("Wow we're connected to the game server!")
+  });
+
+  conn.on("connect", () => {
+    console.log("Name: MCD")
+  });
+
+  
   conn.on("data", (data) => {
     console.log(data);
   });
+  
+
 
   conn.setEncoding("utf8");
 
